3f03e931a83e477c72e5aff6e01e9edc8c0c5a57
GuiTsuke <guilhermebborges04m2@gmail.com>
2025-05-02 20:44:59 -0300

    Ajuste de Constantes de API

---
+++ ScreenSound.Web/Constantes.cs
@@ -0,0 +1,7 @@
+﻿namespace ScreenSound.Web
+{
+    public static class Constantes
+    {
+        public const string ENDPOINT_API = "https://three506-csharpweb-screensound-curso1.onrender.com/";
+    }
+}
